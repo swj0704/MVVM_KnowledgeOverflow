7914428c5fe0f1d518eb764610a2e3dbd1987c7c
structsize <52183019+structsize@users.noreply.github.com>
2021-01-13 00:05:42 +0900

    [UPDATE] create sign up

---
+++ knowledgeoverflow/app/src/main/java/com/example/knowledgeoverflow/view/SignUpActivity.kt
@@ -0,0 +1,29 @@
+package com.example.knowledgeoverflow.view
+
+import com.example.knowledgeoverflow.R
+import com.example.knowledgeoverflow.base.BaseActivity
+import com.example.knowledgeoverflow.databinding.ActivitySignUpBinding
+import com.example.knowledgeoverflow.network.api.AddAPI
+import com.example.knowledgeoverflow.network.api.CheckAPI
+import com.example.knowledgeoverflow.network.api.GetAPI
+import com.example.knowledgeoverflow.viewmodel.SignUpViewModel
+import org.koin.android.ext.android.get
+
+class SignUpActivity : BaseActivity<SignUpViewModel, ActivitySignUpBinding>() {
+
+    private val addAPI : AddAPI = get()
+    private val getAPI : GetAPI = get()
+    private val checkAPI : CheckAPI = get()
+
+    override val resource: Int
+        get() = R.layout.activity_sign_up
+    override val viewModel: SignUpViewModel
+        get() = SignUpViewModel(getAPI, addAPI, checkAPI)
+
+    override fun init() {}
+
+    override fun observerViewModel() {
+
+    }
+
+}
---
+++ knowledgeoverflow/app/src/main/java/com/example/knowledgeoverflow/viewmodel/SignUpViewModel.kt
@@ -0,0 +1,133 @@
+package com.example.knowledgeoverflow.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import com.example.knowledgeoverflow.network.DTO.GetResponse
+import com.example.knowledgeoverflow.network.DTO.UserResponse
+import com.example.knowledgeoverflow.base.BaseViewModel
+import com.example.knowledgeoverflow.network.DTO.Status
+import com.example.knowledgeoverflow.network.api.AddAPI
+import com.example.knowledgeoverflow.network.api.CheckAPI
+import com.example.knowledgeoverflow.network.api.GetAPI
+import com.example.knowledgeoverflow.widget.SingleLiveEvent
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+
+class SignUpViewModel(private val getAPI: GetAPI, private val addAPI: AddAPI, private val checkAPI: CheckAPI) : BaseViewModel() {
+    val nickname = MutableLiveData<String>()
+    val email = MutableLiveData<String>()
+    val password = MutableLiveData<String>()
+    val passwordCheck = MutableLiveData<String>()
+    val onNickNameDuplicateEvent = SingleLiveEvent<Unit>()
+    val onNotNickNameDuplicateEvent = SingleLiveEvent<Unit>()
+    val onNickNameSwearEvent = SingleLiveEvent<Unit>()
+    val onNotNickNameSwearEvent = SingleLiveEvent<Unit>()
+    val onEmailDuplicateEvent = SingleLiveEvent<Unit>()
+    val onNotEmailDuplicateEvent = SingleLiveEvent<Unit>()
+    val onNotPasswordDuplicateEvent = SingleLiveEvent<Unit>()
+    var isDuplicateNickname : Boolean = true
+    var isSwearNickname : Boolean = true
+    var isDuplicateEmail : Boolean = true
+
+    fun checkEmail(){
+        if(checkText(email.value)) {
+            getAPI.getUser(email = email.value).enqueue(object : Callback<GetResponse<UserResponse>> {
+                override fun onResponse(
+                        call: Call<GetResponse<UserResponse>>,
+                        response: Response<GetResponse<UserResponse>>
+                ) {
+                    isDuplicateEmail = if (response.body()!!.status == 200) {
+                        onEmailDuplicateEvent.call()
+                        true
+                    } else {
+                        onNotEmailDuplicateEvent.call()
+                        false
+                    }
+                }
+
+                override fun onFailure(call: Call<GetResponse<UserResponse>>, t: Throwable) {
+                    onErrorEvent.call()
+                }
+
+            })
+        }
+    }
+
+    fun checkNickName(){
+        if(checkText(nickname.value)) {
+            getAPI.getUser(name = nickname.value).enqueue(object : Callback<GetResponse<UserResponse>> {
+                override fun onResponse(
+                        call: Call<GetResponse<UserResponse>>,
+                        response: Response<GetResponse<UserResponse>>
+                ) {
+                    if (response.body()!!.status == 200) {
+                        onNickNameDuplicateEvent.call()
+                        isDuplicateNickname = true
+                    } else {
+                        isDuplicateNickname = false
+                        nickname.value?.let { checkAPI.checkSwearing(it).enqueue(object : Callback<GetResponse<String>>{
+                            override fun onResponse(call: Call<GetResponse<String>>, response: Response<GetResponse<String>>) {
+                                if(response.body()!!.status == 200){
+                                    isSwearNickname = true
+                                    onNickNameSwearEvent.call()
+                                } else {
+                                    isSwearNickname = false
+                                    onNotNickNameSwearEvent.call()
+                                }
+                            }
+
+                            override fun onFailure(call: Call<GetResponse<String>>, t: Throwable) {
+                                onErrorEvent.call()
+                            }
+
+                        }) }
+                    }
+                }
+
+                override fun onFailure(call: Call<GetResponse<UserResponse>>, t: Throwable) {
+                    onErrorEvent.call()
+                }
+
+            })
+        }
+    }
+
+    fun signUp(){
+        if(isDuplicateEmail || isDuplicateNickname){
+            onFailEvent.call()
+        } else {
+            if(password.value != null) {
+                if (password.value == passwordCheck.value) {
+                    addAPI.addUser(email = email.value!!, name = nickname.value!!, password = password.value!!)
+                        .enqueue(object : Callback<Status> {
+                            override fun onResponse(
+                                call: Call<Status>,
+                                response: Response<Status>
+                            ) {
+                                if(response.body()!!.status == 200){
+                                    onSuccessEvent.call()
+                                } else {
+                                    onFailEvent.call()
+                                }
+                            }
+
+                            override fun onFailure(call: Call<Status>, t: Throwable) {
+                                onErrorEvent.call()
+                            }
+
+                        })
+                } else {
+                    onNotPasswordDuplicateEvent.call()
+                }
+            } else {
+                onFailEvent.call()
+            }
+        }
+    }
+
+    private fun checkText(text : String?) : Boolean{
+        return text?.isNotEmpty() ?: false
+    }
+
+}
