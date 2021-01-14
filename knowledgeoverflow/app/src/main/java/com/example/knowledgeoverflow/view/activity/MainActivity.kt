aa3f774aebeb7c729dc508d666061bf8f1aa4313
structsize <52183019+structsize@users.noreply.github.com>
2021-01-14 09:09:12 +0900

    [UPDATE] add view binding

--- knowledgeoverflow/app/src/main/java/com/example/knowledgeoverflow/view/activity/MainActivity.kt
+++ knowledgeoverflow/app/src/main/java/com/example/knowledgeoverflow/view/activity/MainActivity.kt
@@ -2,14 +2,17 @@ package com.example.knowledgeoverflow.view.activity
 
 import android.view.Menu
 import androidx.appcompat.widget.Toolbar
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.knowledgeoverflow.BR
 import com.example.knowledgeoverflow.R
 import com.example.knowledgeoverflow.base.BaseActivity
 import com.example.knowledgeoverflow.databinding.ActivityMainBinding
+import com.example.knowledgeoverflow.databinding.MainIncludeDrawerBinding
 import com.example.knowledgeoverflow.viewmodel.activity.MainViewModel
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.nav_header_main.view.*
@@ -21,24 +24,30 @@ class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>() {
     override val resource: Int
         get() = R.layout.activity_main
 
+    lateinit var back: MainIncludeDrawerBinding
+
     override val viewModel: MainViewModel
         get() = MainViewModel(this@MainActivity)
 
     override fun init() {
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
 
-        val navView: NavigationView = findViewById(R.id.navigation_view)
+        back = DataBindingUtil.setContentView(this, R.layout.main_include_drawer)
+        back.setVariable(BR.viewModel, viewModel)
+        back.lifecycleOwner = this
+        back.executePendingBindings()
+
+        setSupportActionBar(binding.toolbar)
+
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
 
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        back.navigationView.setupWithNavController(navController)
     }
 
     override fun observerViewModel() {
-        TODO("Not yet implemented")
+        
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
