15e103470c31a22e12fa5bafc65b3bffad871818
structsize <52183019+structsize@users.noreply.github.com>
2021-01-15 22:51:20 +0900

    [UPDATE] update main acitivty

--- knowledgeoverflow/app/src/main/java/com/example/knowledgeoverflow/view/activity/MainActivity.kt
+++ knowledgeoverflow/app/src/main/java/com/example/knowledgeoverflow/view/activity/MainActivity.kt
@@ -2,48 +2,53 @@ package com.example.knowledgeoverflow.view.activity
 
 import android.view.Menu
 import androidx.appcompat.widget.Toolbar
-import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.example.knowledgeoverflow.BR
 import com.example.knowledgeoverflow.R
 import com.example.knowledgeoverflow.base.BaseActivity
 import com.example.knowledgeoverflow.databinding.ActivityMainBinding
-import com.example.knowledgeoverflow.databinding.MainIncludeDrawerBinding
 import com.example.knowledgeoverflow.viewmodel.activity.MainViewModel
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.nav_header_main.view.*
+import org.koin.androidx.viewmodel.ext.android.getViewModel
 
 class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
 
     override val resource: Int
-        get() = R.layout.activity_main
+        get() = R.layout.main_include_drawer
 
-    lateinit var back: MainIncludeDrawerBinding
 
     override val viewModel: MainViewModel
-        get() = MainViewModel(this@MainActivity)
+        get() = getViewModel(MainViewModel::class)
 
     override fun init() {
 
-        back = DataBindingUtil.setContentView(this, R.layout.main_include_drawer)
-        back.setVariable(BR.viewModel, viewModel)
-        back.lifecycleOwner = this
-        back.executePendingBindings()
+        val navigationView : NavigationView = findViewById(R.id.navigation_view)
 
-        setSupportActionBar(binding.toolbar)
+        val toolbar : Toolbar = findViewById(R.id.toolbar)
+        val drawerLayout: DrawerLayout = findViewById(R.id.main_drawer_layout)
+        setSupportActionBar(toolbar)
 
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
 
+        appBarConfiguration = AppBarConfiguration(
+                setOf(
+                        R.id.navigation_home, R.id.navigation_slideshow, R.id.navigation_setting
+                ), drawerLayout
+        )
+
+        setText()
+
         setupActionBarWithNavController(navController, appBarConfiguration)
-        back.navigationView.setupWithNavController(navController)
+        navigationView.setupWithNavController(navController)
     }
 
     override fun observerViewModel() {
@@ -60,5 +65,11 @@ class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>() {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
+    private fun setText(){
+        val navView: NavigationView = findViewById(R.id.navigation_view)
+        val header = navView.getHeaderView(0)
 
+        header.sideEmailText.text = viewModel.email
+        header.sideNickNameText.text = viewModel.nickName
+    }
 }
