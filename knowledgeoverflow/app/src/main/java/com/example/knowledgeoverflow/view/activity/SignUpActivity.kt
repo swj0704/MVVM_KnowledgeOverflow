b9aa4a47b26daba08c8aee383b2242c6b316de50
structsize <52183019+structsize@users.noreply.github.com>
2021-01-15 22:52:02 +0900

    [UPDATE] update sign up activity

--- knowledgeoverflow/app/src/main/java/com/example/knowledgeoverflow/view/activity/SignUpActivity.kt
+++ knowledgeoverflow/app/src/main/java/com/example/knowledgeoverflow/view/activity/SignUpActivity.kt
@@ -1,5 +1,8 @@
 package com.example.knowledgeoverflow.view.activity
 
+import android.content.Intent
+import android.widget.Toast
+import cn.pedant.SweetAlert.SweetAlertDialog
 import com.example.knowledgeoverflow.R
 import com.example.knowledgeoverflow.base.BaseActivity
 import com.example.knowledgeoverflow.databinding.ActivitySignUpBinding
@@ -8,22 +11,69 @@ import com.example.knowledgeoverflow.network.api.CheckAPI
 import com.example.knowledgeoverflow.network.api.GetAPI
 import com.example.knowledgeoverflow.viewmodel.activity.SignUpViewModel
 import org.koin.android.ext.android.get
+import org.koin.androidx.viewmodel.ext.android.getViewModel
 
 class SignUpActivity : BaseActivity<SignUpViewModel, ActivitySignUpBinding>() {
-
-    private val addAPI : AddAPI = get()
-    private val getAPI : GetAPI = get()
-    private val checkAPI : CheckAPI = get()
 
     override val resource: Int
         get() = R.layout.activity_sign_up
     override val viewModel: SignUpViewModel
-        get() = SignUpViewModel(getAPI, addAPI, checkAPI)
+        get() = getViewModel(SignUpViewModel::class)
 
     override fun init() {}
 
     override fun observerViewModel() {
+        with(viewModel){
+            onEmailDuplicateEvent.observe(this@SignUpActivity, {
+                binding.signUpIDLayout.error = "중복된 이메일입니다"
+            })
+            onNotEmailDuplicateEvent.observe(this@SignUpActivity, {
+                binding.signUpIDLayout.error = null
+            })
+            onNickNameDuplicateEvent.observe(this@SignUpActivity, {
+                binding.signUpNameLayout.error = "중복된 닉네임입니다"
+            })
+            onNickNameSwearEvent.observe(this@SignUpActivity, {
+                binding.signUpNameLayout.error = "욕설 사용 금지"
+            })
+            onCheckNickname.observe(this@SignUpActivity, {
+                binding.signUpNameLayout.error = null
+            })
+            onSuccessEvent.observe(this@SignUpActivity, {
+                val dialog = SweetAlertDialog(this@SignUpActivity, SweetAlertDialog.SUCCESS_TYPE)
 
+                dialog.setCancelable(false)
+
+                dialog.setTitleText("회원가입이 성공하였습니다")
+                        .setConfirmClickListener {
+                            dialog.dismiss()
+                            finish()
+                        }
+                        .show()
+            })
+            onFailEvent.observe(this@SignUpActivity, {
+                val dialog = SweetAlertDialog(this@SignUpActivity, SweetAlertDialog.ERROR_TYPE)
+
+                dialog.setCancelable(false)
+
+                dialog.setTitleText("회원가입이 실패하였습니다")
+                        .setConfirmClickListener {
+                            dialog.dismiss()
+                        }
+                        .show()
+            })
+            onErrorEvent.observe(this@SignUpActivity, {
+                val dialog = SweetAlertDialog(this@SignUpActivity, SweetAlertDialog.ERROR_TYPE)
+
+                dialog.setCancelable(false)
+
+                dialog.setTitleText("오류가 발생하였습니다")
+                        .setConfirmClickListener {
+                            dialog.dismiss()
+                        }
+                        .show()
+            })
+        }
     }
 
 }
