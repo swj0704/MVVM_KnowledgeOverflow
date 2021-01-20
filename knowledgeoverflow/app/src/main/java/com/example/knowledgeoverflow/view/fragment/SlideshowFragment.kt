9fc4df8ef6fcb81e922a76e20f9c7f2c1db6565a
structsize <52183019+structsize@users.noreply.github.com>
2021-01-21 00:23:25 +0900

    [UPDATE] update spinner

--- knowledgeoverflow/app/src/main/java/com/example/knowledgeoverflow/view/fragment/SlideshowFragment.kt
+++ knowledgeoverflow/app/src/main/java/com/example/knowledgeoverflow/view/fragment/SlideshowFragment.kt
@@ -1,8 +1,10 @@
 package com.example.knowledgeoverflow.view.fragment
 
+import android.widget.ArrayAdapter
 import com.example.knowledgeoverflow.R
 import com.example.knowledgeoverflow.base.BaseFragment
 import com.example.knowledgeoverflow.databinding.FragmentSlideshowBinding
+import com.example.knowledgeoverflow.view.activity.MainActivity
 import com.example.knowledgeoverflow.viewmodel.fragment.SlideshowViewModel
 import org.koin.androidx.viewmodel.ext.android.getViewModel
 
@@ -14,7 +16,14 @@ class SlideshowFragment : BaseFragment<FragmentSlideshowBinding, SlideshowViewMo
         get() = getViewModel(SlideshowViewModel::class)
 
     override fun init() {
+        val spinnerArray: MutableList<String> = ArrayList()
+        spinnerArray.add("상식")
+        spinnerArray.add("코딩")
+        spinnerArray.add("일상")
+        val adapter = ArrayAdapter<String>(viewModel.mContext, android.R.layout.simple_spinner_item, spinnerArray)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
+        binding.themeSpinner.adapter = adapter
     }
 
     override fun observerViewModel() {
