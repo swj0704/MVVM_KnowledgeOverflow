764415a61fdbfd8caa51674c8d53a292c114240e
structsize <52183019+structsize@users.noreply.github.com>
2021-01-22 23:18:39 +0900

    [UPDATE] update spinner

--- knowledgeoverflow/app/src/main/java/com/example/knowledgeoverflow/view/fragment/SlideshowFragment.kt
+++ knowledgeoverflow/app/src/main/java/com/example/knowledgeoverflow/view/fragment/SlideshowFragment.kt
@@ -1,6 +1,11 @@
 package com.example.knowledgeoverflow.view.fragment
 
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.SearchView
 import com.example.knowledgeoverflow.R
 import com.example.knowledgeoverflow.base.BaseFragment
 import com.example.knowledgeoverflow.databinding.FragmentSlideshowBinding
@@ -15,7 +20,11 @@ class SlideshowFragment : BaseFragment<FragmentSlideshowBinding, SlideshowViewMo
     override val viewModel: SlideshowViewModel
         get() = getViewModel(SlideshowViewModel::class)
 
+    var item : String = ""
+
     override fun init() {
+        setHasOptionsMenu(true)
+
         val spinnerArray: MutableList<String> = ArrayList()
         spinnerArray.add("상식")
         spinnerArray.add("코딩")
@@ -24,6 +33,17 @@ class SlideshowFragment : BaseFragment<FragmentSlideshowBinding, SlideshowViewMo
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
         binding.themeSpinner.adapter = adapter
+        binding.themeSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                item = spinnerArray[position]
+                viewModel.getList(spinnerArray[position])
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                item = spinnerArray[0]
+                viewModel.getList("상식")
+            }
+        }
     }
 
     override fun observerViewModel() {
@@ -33,6 +53,29 @@ class SlideshowFragment : BaseFragment<FragmentSlideshowBinding, SlideshowViewMo
             })
         }
     }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        val search = menu.findItem(R.id.action_search)
+        val searchView : SearchView = search.actionView as SearchView
+        searchView.queryHint = "제목 검색"
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(s: String?): Boolean {
+                if (s != null) {
+                    if(s.isNotEmpty()) {
+                        viewModel.getList(item, s)
+                    }
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                return false
+            }
+
+        })
+    }
+
 
 
 }
