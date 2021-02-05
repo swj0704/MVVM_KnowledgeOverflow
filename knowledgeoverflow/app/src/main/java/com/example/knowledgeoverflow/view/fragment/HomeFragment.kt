4546315ac6ddbbcb8ac1d522b292345af900852e
structsize <52183019+structsize@users.noreply.github.com>
2021-02-05 23:48:13 +0900

    [UPDATE] make event

--- knowledgeoverflow/app/src/main/java/com/example/knowledgeoverflow/view/fragment/HomeFragment.kt
+++ knowledgeoverflow/app/src/main/java/com/example/knowledgeoverflow/view/fragment/HomeFragment.kt
@@ -1,9 +1,12 @@
 package com.example.knowledgeoverflow.view.fragment
 
+import android.content.Context
 import com.example.knowledgeoverflow.R
 import com.example.knowledgeoverflow.base.BaseFragment
 import com.example.knowledgeoverflow.databinding.FragmentHomeBinding
+import com.example.knowledgeoverflow.view.activity.LoginActivity
 import com.example.knowledgeoverflow.viewmodel.fragment.HomeViewModel
+import com.example.knowledgeoverflow.widget.extention.startActivityWithFinish
 import org.koin.androidx.viewmodel.ext.android.getViewModel
 
 class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>() {
@@ -17,7 +20,24 @@ class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>() {
     }
 
     override fun observerViewModel() {
+        with(viewModel){
+            onLogoutEvent.observe(this@HomeFragment, {
+                delete()
+                activity?.let { it1 -> startActivityWithFinish(it1, LoginActivity::class.java) }
+            })
+            onQuestionEvent.observe(this@HomeFragment, {
 
+            })
+            onAnswerEvent.observe(this@HomeFragment, {
+
+            })
+            onCommentEvent.observe(this@HomeFragment, {
+
+            })
+            onChangeEvent.observe(this@HomeFragment,{
+                
+            })
+        }
     }
 
 }
