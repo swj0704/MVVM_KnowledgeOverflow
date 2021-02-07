3b5503e67f4a5a80a59b62796eadd67c783f131e
structsize <52183019+structsize@users.noreply.github.com>
2021-02-07 23:45:09 +0900

    [UPDATE] update event call

--- knowledgeoverflow/app/src/main/java/com/example/knowledgeoverflow/view/fragment/SettingFragment.kt
+++ knowledgeoverflow/app/src/main/java/com/example/knowledgeoverflow/view/fragment/SettingFragment.kt
@@ -3,7 +3,9 @@ package com.example.knowledgeoverflow.view.fragment
 import com.example.knowledgeoverflow.R
 import com.example.knowledgeoverflow.base.BaseFragment
 import com.example.knowledgeoverflow.databinding.FragmentSettingBinding
+import com.example.knowledgeoverflow.view.activity.LoginActivity
 import com.example.knowledgeoverflow.viewmodel.fragment.SettingViewModel
+import com.example.knowledgeoverflow.widget.extention.startActivityWithFinish
 import org.koin.androidx.viewmodel.ext.android.getViewModel
 
 class SettingFragment : BaseFragment<FragmentSettingBinding, SettingViewModel>() {
@@ -18,7 +20,24 @@ class SettingFragment : BaseFragment<FragmentSettingBinding, SettingViewModel>()
     }
 
     override fun observerViewModel() {
+        with(viewModel){
+            onLogoutEvent.observe(this@SettingFragment, {
+                delete()
+                activity?.let { it1 -> startActivityWithFinish(it1, LoginActivity::class.java) }
+            })
+            onQuestionEvent.observe(this@SettingFragment, {
 
+            })
+            onAnswerEvent.observe(this@SettingFragment, {
+
+            })
+            onCommentEvent.observe(this@SettingFragment, {
+
+            })
+            onChangeEvent.observe(this@SettingFragment, {
+
+            })
+        }
     }
 
 
